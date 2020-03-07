e169aa44e3a256b34ed8eef12993fdbbb3bd09c3
PTL <jhiou01@hotmail.com.tw>
2020-03-07 15:05:22 +0800

    fin

---
+++ app/src/main/java/tw/org/iii/brad/brad02/WelcomeActivity.java
@@ -0,0 +1,31 @@
+package tw.org.iii.brad.brad02;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class WelcomeActivity extends AppCompatActivity {
+    private View view;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+
+        view = findViewById(R.id.welcome);
+        view.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                gotoMain();
+            }
+        });
+    }
+
+    private void gotoMain(){
+        Intent intent = new Intent(this, MainActivity.class);//從這跳到MainActivity的物件實體
+        startActivity(intent);
+        finish();
+    }
+}
